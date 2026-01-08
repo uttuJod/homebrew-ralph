18c2840683a12f2a615051024d83239743649b50
utkarsh <utkarshtiwari1603@gmail.com>
2026-01-08 22:57:08 +0000

    Update ralph-orchestrator.rb

--- Formula/ralph-orchestrator.rb
+++ Formula/ralph-orchestrator.rb
@@ -8,6 +8,11 @@ class RalphOrchestrator < Formula
   license "MIT"
 
   depends_on "python@3.11"
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/source/P/PyYAML/PyYAML-6.0.3.tar.gz"
+    sha256 "d5eef459c36e42f3e8d95c87ddf7a55b2a0920d28f9e7e9f3b7c4d6c9e34b34d"
+  end
+
 
   def install
     virtualenv_install_with_resources
