df677518a52b20cec5f5a90aed542832bd722a8a
utkarsh <utkarshtiwari1603@gmail.com>
2026-01-08 23:14:15 +0000

    Update ralph-orchestrator.rb

--- Formula/ralph-orchestrator.rb
+++ Formula/ralph-orchestrator.rb
@@ -4,7 +4,7 @@ class RalphOrchestrator < Formula
   desc "AI agent orchestrator using the Ralph Wiggum technique"
   homepage "https://github.com/mikeyobrien/ralph-orchestrator"
   url "https://files.pythonhosted.org/packages/83/2c/6f08f20a75fa01f558d1ba2436d7545defb3c4bc28cd16bfa6e50631bb1a/ralph_orchestrator-1.2.2.tar.gz"
-  sha256 "FCCE6BD712AA4D17925B52C482C274B8D63CB971DBB2444B4F1D367B9022EA75"
+  sha256 "fcce6bd712aa4d17925b52c482c274b8d63cb971dbb2444b4f1d367b9022ea75"
   license "MIT"
 
   depends_on "python@3.11"
